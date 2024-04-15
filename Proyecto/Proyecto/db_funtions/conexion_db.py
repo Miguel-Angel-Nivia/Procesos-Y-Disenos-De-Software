7facb42dd7185aabfe22a3ab126de77e19f5b12e
Willian Chapid <willian@DESKTOP-T03R466>
2024-04-14 19:20:57 -0500

    Creacion menu inicial y base de datos

--- Proyecto/Proyecto/db_funtions/conexion_db.py
+++ Proyecto/Proyecto/db_funtions/conexion_db.py
@@ -7,17 +7,15 @@ class Comunicacion():
         self.cursor = self.conexion.cursor()
 
     def inicio_base(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS LOGSISTEMA ( fecha TIMESTAMP, mensaje TEXT)")
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS usuarios (
                 id INTEGER PRIMARY KEY,
                 nombre TEXT NOT NULL,
                 apellido TEXT NOT NULL,
-                rol TEXT NOT NULL,
-                email TEXT UNIQUE NOT NULL
+                email TEXT UNIQUE NOT NULL,
+                telefono INTEGER NOT NULL
             )
         """)
-
         # Create the 'dispositivos' table
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS dispositivos (
@@ -26,7 +24,24 @@ class Comunicacion():
                 estado TEXT NOT NULL
             )
         """)
-
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS administradores(
+                id INTEGER PRIMARY KEY,
+                cc INTEGER NOT NULL,
+                usuario TEXT NOT NULL,
+                contrasenia TEXT NOT NULL,
+                FOREIGN KEY (cc) REFERENCES usuarios(id)
+            )
+        """
+        )
+        self.cursor.execute("""
+                CREATE TABLE IF NOT EXISTS LOGSISTEMA ( 
+                    fecha TIMESTAMP NOT NULL,
+                    usuario TEXT NOT NULL,
+                    mensaje TEXT NOT NULL,
+                    FOREIGN KEY (usuario) REFERENCES administradores(usuario)
+                    )
+        """)
         # Create the 'pedidos' table
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS pedidos (
@@ -36,9 +51,10 @@ class Comunicacion():
                 id_usuario_receptor INTEGER NOT NULL,
                 tipo_encargo TEXT NOT NULL,
                 descripcion TEXT NOT NULL,
-                fecha_solicitud DATE NOT NULL,
-                fecha_recepcion DATE,
+                fecha_solicitud TIMESTAMP NOT NULL,
+                fecha_recepcion TIMESTAMP,
                 lugar_recepcion TEXT,
+                url TEXT NOT NULL,
                 FOREIGN KEY (id_dispositivo) REFERENCES dispositivos(id),
                 FOREIGN KEY (id_usuario_envio) REFERENCES usuarios(id),
                 FOREIGN KEY (id_usuario_receptor) REFERENCES usuarios(id)
